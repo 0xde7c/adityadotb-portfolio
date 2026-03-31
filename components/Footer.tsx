3e895a3a9f885b43e4b1a115af2fefecb56ebbe0
0xde7c <0xde7c@gmail.com>
2026-04-01 06:31:51 +0800

    Remove footer copyright

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,3 @@
 export function Footer() {
-  const year = new Date().getFullYear();
-  return (
-    <footer className="bg-white text-black">
-      <div className="max-w-4xl mx-auto px-4 py-8 font-mono text-sm text-center text-gray-700">
-        <p>&copy; {year} aditya. all rights reserved.</p>
-      </div>
-    </footer>
-  );
+  return null;
 }
